f9689fbbaf5adb295a4ac9bd5e5241dad50ff18a
Vincent Sukamto <vincentsukamto0311@gmail.com>
2022-02-14 12:59:12 +0700

    renaming titles and uppercase for aside user name

--- app/Views/layout/aside.php
+++ app/Views/layout/aside.php
@@ -56,7 +56,7 @@
           <img src="<?php echo base_url() ?>/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block"><?php echo $_SESSION['name']; ?></a>
+          <a href="#" class="d-block"><?php echo ucwords($_SESSION['name']); ?></a>
         </div>
       </div>
 
